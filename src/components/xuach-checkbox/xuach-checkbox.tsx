7bbfceca14a8f34859d5d94b3a87a5e1f19a7ce0
AlvaroJbucaro <alvarojbucaro@gmail.com>
2021-07-11 18:51:24 -0500

    Feature/Xuach-80

--- src/components/xuach-checkbox/xuach-checkbox.tsx
+++ src/components/xuach-checkbox/xuach-checkbox.tsx
@@ -9,14 +9,27 @@ import { CONST_IMAGES_CHECKBOX } from '../../const/constCheckBox';
 export class XuachCheckbox {
   @Prop() checked: boolean = false;
   @Prop() disabled: boolean = false;
-  @Prop() size: number = 16;
+  @Prop() size: number = 24;
 
+  //Event to emit any action from of parent
   @Event() clickCheckBox: EventEmitter;
+  eventCheckBox(ev: Event) {
+    ev.preventDefault();
+    this.clickCheckBox.emit();
+  }
 
+  //get background images for checkbox states
   getBackgroundImageCheckBox() {
     const backgroundImageCheckBox = this.disabled ? CONST_IMAGES_CHECKBOX['DISABLED'] : CONST_IMAGES_CHECKBOX['CHECKED'];
     return backgroundImageCheckBox;
   }
+
+  //get object css for checkbox "size and image"
+    styleCheckbox = {
+      backgroundImage: `url(${this.getBackgroundImageCheckBox()})`,
+      height: `${this.size}px`,
+      width: `${this.size}px`,
+    };
 
   render() {
     return (
@@ -24,10 +37,10 @@ export class XuachCheckbox {
         <label class="checkbox">
           <input
             type="checkbox"
-            style={{ backgroundImage: `url(${this.getBackgroundImageCheckBox()})` }}
             checked={this.checked}
             disabled={this.disabled}
-            onClick={this.clickCheckBox.emit}
+            onClick={this.eventCheckBox.bind(this)}
+            style={this.styleCheckbox}
           />
         </label>
       </Host>
