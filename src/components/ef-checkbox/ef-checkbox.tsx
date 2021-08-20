7a3647727f9d88c5b90b127f95f87d58b935b74f
alvarojbucaro <53539479+alvarojbucaro@users.noreply.github.com>
2021-08-20 16:05:34 -0500

    XCH-50-MODAL-COMPONENT (#11)
    * XCH-50-MODAL-COMPONENT
    
    * build react
    
    * update checkbox
    
    * 0.5.0

--- src/components/ef-checkbox/ef-checkbox.tsx
+++ src/components/ef-checkbox/ef-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Event, EventEmitter } from '@stencil/core';
+import { Component, Host, h, Prop, Event, EventEmitter, State } from '@stencil/core';
 import { CONST_IMAGES_CHECKBOX } from './constCheckBox';
 
 @Component({
@@ -11,6 +11,8 @@ export class XuachCheckbox {
   @Prop() disabled: boolean = false;
   @Prop() size: number = 24;
 
+  @State() styleCheckbox: { backgroundImage: string; height: string; width: string };
+
   //Event to emit any action from of parent
   @Event() event: EventEmitter;
   eventCheckBox(ev: Event) {
@@ -18,17 +20,27 @@ export class XuachCheckbox {
     this.event.emit();
   }
 
-  //get background images for checkbox states
-  getBackgroundImageCheckBox() {
-    return this.disabled ? CONST_IMAGES_CHECKBOX['DISABLED'] : CONST_IMAGES_CHECKBOX['CHECKED'];
+  //function for load state info
+  componentWillLoad() {
+    console.log(CONST_IMAGES_CHECKBOX['DISABLED']);
+    this.styleCheckbox = {
+      backgroundImage: `url(${this.getBackgroundImageCheckBox()})`,
+      height: `${this.size}px`,
+      width: `${this.size}px`,
+    };
+  }
+  componentWillUpdate() {
+    this.styleCheckbox = {
+      ...this.styleCheckbox,
+      backgroundImage: `url(${this.getBackgroundImageCheckBox()})`,
+    };
   }
 
-  //get object css for checkbox "size and image"
-  styleCheckbox = {
-    backgroundImage: `url(${this.getBackgroundImageCheckBox()})`,
-    height: `${this.size}px`,
-    width: `${this.size}px`,
-  };
+  //get background images for checkbox states
+  getBackgroundImageCheckBox() {
+    if (this.disabled) return CONST_IMAGES_CHECKBOX['DISABLED'];
+    return this.checked ? CONST_IMAGES_CHECKBOX['CHECKED'] : '';
+  }
 
   render() {
     return (
