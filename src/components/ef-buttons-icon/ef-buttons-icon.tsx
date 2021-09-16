3eb5c3d40fd5201cc5d9ab35347df4c9c8856f36
AlvaroJbucaro <alvarojbucaro@gmail.com>
2021-09-16 15:14:49 -0500

    Update ef-buttons-icon.tsx

--- src/components/ef-buttons-icon/ef-buttons-icon.tsx
+++ src/components/ef-buttons-icon/ef-buttons-icon.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h, Prop, Event, EventEmitter } from '@stencil/core';
+import { Component, h, Prop, Event, EventEmitter } from '@stencil/core';
 
 @Component({
   tag: 'ef-buttons-icon',
