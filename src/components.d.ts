c169feadc541f1912576390a6c6b715bfd81fd33
alvarojbucaro <53539479+alvarojbucaro@users.noreply.github.com>
2021-09-17 12:28:51 -0500

    XCH-178-BUTTONS-ICON (#22)
    * XCH-178-BUTTONS-ICON
    
    * Update ef-buttons-icon.tsx
    
    * Update ef-template-admin.css

--- src/components.d.ts
+++ src/components.d.ts
@@ -13,6 +13,10 @@ export namespace Components {
         "size": 'LARGE' | 'MEDIUM' | 'SMALL';
         "text": string;
     }
+    interface EfButtonsIcon {
+        "align": 'HORIZONTAL' | 'VERTICAL';
+        "buttons": { urlIcon: string; id: string }[];
+    }
     interface EfCardInfo {
         "image": string;
         "styleTitle": { [key: string]: string };
@@ -114,6 +118,12 @@ declare global {
         prototype: HTMLEfButtonElement;
         new (): HTMLEfButtonElement;
     };
+    interface HTMLEfButtonsIconElement extends Components.EfButtonsIcon, HTMLStencilElement {
+    }
+    var HTMLEfButtonsIconElement: {
+        prototype: HTMLEfButtonsIconElement;
+        new (): HTMLEfButtonsIconElement;
+    };
     interface HTMLEfCardInfoElement extends Components.EfCardInfo, HTMLStencilElement {
     }
     var HTMLEfCardInfoElement: {
@@ -206,6 +216,7 @@ declare global {
     };
     interface HTMLElementTagNameMap {
         "ef-button": HTMLEfButtonElement;
+        "ef-buttons-icon": HTMLEfButtonsIconElement;
         "ef-card-info": HTMLEfCardInfoElement;
         "ef-checkbox": HTMLEfCheckboxElement;
         "ef-chip": HTMLEfChipElement;
@@ -231,6 +242,11 @@ declare namespace LocalJSX {
         "onEvent"?: (event: CustomEvent<any>) => void;
         "size"?: 'LARGE' | 'MEDIUM' | 'SMALL';
         "text"?: string;
+    }
+    interface EfButtonsIcon {
+        "align"?: 'HORIZONTAL' | 'VERTICAL';
+        "buttons"?: { urlIcon: string; id: string }[];
+        "onEvent"?: (event: CustomEvent<string>) => void;
     }
     interface EfCardInfo {
         "image"?: string;
@@ -336,6 +352,7 @@ declare namespace LocalJSX {
     }
     interface IntrinsicElements {
         "ef-button": EfButton;
+        "ef-buttons-icon": EfButtonsIcon;
         "ef-card-info": EfCardInfo;
         "ef-checkbox": EfCheckbox;
         "ef-chip": EfChip;
@@ -358,6 +375,7 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "ef-button": LocalJSX.EfButton & JSXBase.HTMLAttributes<HTMLEfButtonElement>;
+            "ef-buttons-icon": LocalJSX.EfButtonsIcon & JSXBase.HTMLAttributes<HTMLEfButtonsIconElement>;
             "ef-card-info": LocalJSX.EfCardInfo & JSXBase.HTMLAttributes<HTMLEfCardInfoElement>;
             "ef-checkbox": LocalJSX.EfCheckbox & JSXBase.HTMLAttributes<HTMLEfCheckboxElement>;
             "ef-chip": LocalJSX.EfChip & JSXBase.HTMLAttributes<HTMLEfChipElement>;
---
+++ src/components/ef-buttons-icon/ef-buttons-icon.tsx
@@ -0,0 +1,31 @@
+import { Component, h, Prop, Event, EventEmitter } from '@stencil/core';
+
+@Component({
+  tag: 'ef-buttons-icon',
+  styleUrl: 'ef-buttons-icon.css',
+  shadow: true,
+})
+export class EfButtonsIcon {
+  @Prop() buttons: { urlIcon: string; id: string }[];
+  @Prop() align: 'HORIZONTAL' | 'VERTICAL' = 'HORIZONTAL';
+
+  //Event to emit any action from of parent
+  @Event() event: EventEmitter<string>;
+  eventButton(id: string) {
+    console.log(id);
+    this.event.emit(id);
+  }
+
+  getAlign() {
+    return this.align == 'HORIZONTAL' ? 'ef-buttons-icon_container-horizontal' : 'ef-buttons-icon_container-vertical';
+  }
+
+  renderButtons() {
+    return this.buttons.map(item => {
+      return <img class="ef-buttons-icon_button" onClick={this.eventButton.bind(this, item.id)} src={item.urlIcon} alt={item.id} />;
+    });
+  }
+  render() {
+    return <div class={this.getAlign()}>{this.renderButtons()}</div>;
+  }
+}
